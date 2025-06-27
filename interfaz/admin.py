1e9cdaed85ec3749a1ca2ea5f4a806b45a1dbcc6
jensen27718 <jensenprueba68@gmail.com>
2025-06-27 15:06:24 -0500

    Merge main into work and resolve conflicts

--- interfaz/admin.py
+++ interfaz/admin.py
@@ -1,6 +1,6 @@
 # interfaz/admin.py
 from django.contrib import admin
-from .models import Categoria, Cuenta
+from .models import Categoria, Cuenta, Cliente, Registro
 
 @admin.register(Categoria)
 class CategoriaAdmin(admin.ModelAdmin):
@@ -11,3 +11,20 @@ class CategoriaAdmin(admin.ModelAdmin):
 class CuentaAdmin(admin.ModelAdmin):
     list_display = ('nombre',)
     search_fields = ('nombre',)
+
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
+    search_fields = ('nombre',)
+
+
+@admin.register(Registro)
+class RegistroAdmin(admin.ModelAdmin):
+    list_display = ('fecha', 'descripcion', 'cliente', 'categoria', 'cuenta', 'ingresos', 'egresos')
+    list_filter = ('cliente', 'categoria', 'cuenta')
+    search_fields = ('descripcion',)
+
+
+
+
