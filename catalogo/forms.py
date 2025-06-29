de37b74831f8d0348c0dbdc6e4d9372056757cb2
jensen27718 <jensenprueba68@gmail.com>
2025-06-29 13:40:58 -0500

    Improve catalog admin UI

--- catalogo/forms.py
+++ catalogo/forms.py
@@ -1,23 +1,36 @@
 from django import forms
 from .models import TipoProducto, Categoria, Producto, VariacionProducto, ValorAtributo
 
-class TipoProductoForm(forms.ModelForm):
+
+class BaseStyledForm(forms.ModelForm):
+    """Add Tailwind friendly classes to widgets."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            cls = field.widget.attrs.get("class", "")
+            field.widget.attrs["class"] = f"border rounded p-2 w-full {cls}".strip()
+
+class TipoProductoForm(BaseStyledForm):
     class Meta:
         model = TipoProducto
         fields = ['nombre', 'descripcion', 'imagen_url']
 
-class CategoriaForm(forms.ModelForm):
+class CategoriaForm(BaseStyledForm):
     class Meta:
         model = Categoria
         fields = ['tipo_producto', 'nombre', 'descripcion', 'imagen_url']
 
-class ProductoForm(forms.ModelForm):
+class ProductoForm(BaseStyledForm):
     class Meta:
         model = Producto
         fields = ['categoria', 'referencia', 'nombre', 'foto_url']
 
-class VariacionProductoForm(forms.ModelForm):
-    valores = forms.ModelMultipleChoiceField(queryset=ValorAtributo.objects.all())
+class VariacionProductoForm(BaseStyledForm):
+    valores = forms.ModelMultipleChoiceField(
+        queryset=ValorAtributo.objects.all(),
+        widget=forms.SelectMultiple(attrs={"class": "border rounded p-2 w-full"}),
+    )
     class Meta:
         model = VariacionProducto
         fields = ['producto', 'precio_base', 'valores']
