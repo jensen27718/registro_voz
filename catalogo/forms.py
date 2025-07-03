b6be52160689d9c97837efe89d8cbbe1e2b09ad4
jensen27718 <jensenprueba68@gmail.com>
2025-07-02 22:47:54 -0500

    Add card layout admin with edit and delete

--- catalogo/forms.py
+++ catalogo/forms.py
@@ -1,5 +1,12 @@
 from django import forms
-from .models import TipoProducto, Categoria, Producto, VariacionProducto, ValorAtributo
+from .models import (
+    TipoProducto,
+    Categoria,
+    Producto,
+    AtributoDef,
+    ValorAtributo,
+    VariacionProducto,
+)
 
 
 
@@ -33,11 +40,48 @@ class ProductoForm(BaseStyledForm):
         fields = ['categoria', 'referencia', 'nombre', 'foto_url']
 
 
+class AtributoDefForm(BaseStyledForm):
+
+    class Meta:
+        model = AtributoDef
+        fields = ['tipo_producto', 'nombre']
+
+
+class ValorAtributoForm(BaseStyledForm):
+
+    class Meta:
+        model = ValorAtributo
+        fields = ['atributo_def', 'valor', 'display']
+
+
 class VariacionProductoForm(BaseStyledForm):
     valores = forms.ModelMultipleChoiceField(
-        queryset=ValorAtributo.objects.all(),
+        queryset=ValorAtributo.objects.none(),
         widget=forms.SelectMultiple(attrs={"class": "border rounded p-2 w-full"}),
     )
+
+    def __init__(self, *args, **kwargs):
+        """Filtra los valores según el producto seleccionado."""
+        super().__init__(*args, **kwargs)
+
+        producto_id = None
+        field_name = 'producto'
+        if self.prefix:
+            field_name = f'{self.prefix}-{field_name}'
+        if self.data.get(field_name):
+            producto_id = self.data.get(field_name)
+        elif self.instance.pk:
+            producto_id = self.instance.producto_id
+
+        if producto_id:
+            try:
+                producto = Producto.objects.select_related('categoria__tipo_producto').get(pk=producto_id)
+                tipo_id = producto.categoria.tipo_producto_id
+                self.fields['valores'].queryset = ValorAtributo.objects.filter(
+                    atributo_def__tipo_producto_id=tipo_id
+                )
+            except Producto.DoesNotExist:
+                self.fields['valores'].queryset = ValorAtributo.objects.none()
 
     class Meta:
         model = VariacionProducto
